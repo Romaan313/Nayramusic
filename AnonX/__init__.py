86c47c1875338b899222326cdf5e01800e42a47b
Romaan313 <108215028+Romaan313@users.noreply.github.com>
2023-08-22 15:36:27 +0530

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -1,8 +1,8 @@
-from AnonX.core.bot import AnonXBot
-from AnonX.core.dir import dirr
-from AnonX.core.git import git
-from AnonX.core.userbot import Userbot
-from AnonX.misc import dbb, heroku, sudo
+from NayraX.core.bot import AnonXBot
+from NayraX.core.dir import dirr
+from NayraX.core.git import git
+from NayraX.core.userbot import Userbot
+from NayraX.misc import dbb, heroku, sudo
 from aiohttp import ClientSession
 
 from .logging import LOGGER
@@ -19,7 +19,7 @@ heroku()
 sudo()
 
 # Clients
-app = AnonXBot()
+app = NayraXBot()
 
 userbot = Userbot()
 
